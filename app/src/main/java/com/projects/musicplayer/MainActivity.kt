61360167f0c1bf33f30d1ec242545d7ab816026c
Aman Raj <amanr11314@gmail.com>
2021-04-02 01:01:40 +0530

    :heavy_plus_sign:bottom nav coroners styling added

--- app/src/main/java/com/projects/musicplayer/MainActivity.kt
+++ app/src/main/java/com/projects/musicplayer/MainActivity.kt
@@ -2,10 +2,27 @@ package com.projects.musicplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.shape.CornerFamily
+import com.google.android.material.shape.MaterialShapeDrawable
 
 class MainActivity : AppCompatActivity() {
+    lateinit var bottomNavigationView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+
+        //get radius
+        val radius = resources.getDimension(R.dimen.radius_small)
+        //get bottom nav background view
+        val bottomNavigationViewBackground =
+            bottomNavigationView.background as MaterialShapeDrawable
+        //manually change background appearance
+        bottomNavigationViewBackground.shapeAppearanceModel =
+            bottomNavigationViewBackground.shapeAppearanceModel.toBuilder()
+                .setAllCorners(CornerFamily.ROUNDED, radius)
+                .build()
     }
 }
