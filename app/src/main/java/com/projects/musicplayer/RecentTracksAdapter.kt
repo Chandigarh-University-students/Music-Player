3104b06654cedd2c03b5e702737b95e2091ba564
Aman Raj <amanr11314@gmail.com>
2021-04-09 01:07:15 +0530

    :heavy_plus_sign: recent tracks adapter added and populated in horizontal linear layout

---
+++ app/src/main/java/com/projects/musicplayer/RecentTracksAdapter.kt
@@ -0,0 +1,96 @@
+package com.projects.musicplayer
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+
+
+class RecentTracksAdapter(context: Context) :
+    RecyclerView.Adapter<RecentTracksAdapter.RecentTrackViewHolder>() {
+
+    val mInflater: LayoutInflater = LayoutInflater.from(context)
+
+    private var totalTracks: Int? = null
+
+    class RecentTrackViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val imgSingleRecentTrack: ImageView = view.findViewById(R.id.imgSingleRecentTrack)
+    }
+
+    /**
+     * Called when RecyclerView needs a new [ViewHolder] of the given type to represent
+     * an item.
+     *
+     *
+     * This new ViewHolder should be constructed with a new View that can represent the items
+     * of the given type. You can either create a new View manually or inflate it from an XML
+     * layout file.
+     *
+     *
+     * The new ViewHolder will be used to display items of the adapter using
+     * [.onBindViewHolder]. Since it will be re-used to display
+     * different items in the data set, it is a good idea to cache references to sub views of
+     * the View to avoid unnecessary [View.findViewById] calls.
+     *
+     * @param parent The ViewGroup into which the new View will be added after it is bound to
+     * an adapter position.
+     * @param viewType The view type of the new View.
+     *
+     * @return A new ViewHolder that holds a View of the given view type.
+     * @see .getItemViewType
+     * @see .onBindViewHolder
+     */
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): RecentTrackViewHolder {
+        val songTrackView: View = mInflater.inflate(R.layout.single_recent_track, parent, false)
+        return RecentTrackViewHolder(songTrackView)
+    }
+
+
+    /**
+     * Called by RecyclerView to display the data at the specified position. This method should
+     * update the contents of the [ViewHolder.itemView] to reflect the item at the given
+     * position.
+     *
+     *
+     * Note that unlike [android.widget.ListView], RecyclerView will not call this method
+     * again if the position of the item changes in the data set unless the item itself is
+     * invalidated or the new position cannot be determined. For this reason, you should only
+     * use the `position` parameter while acquiring the related data item inside
+     * this method and should not keep a copy of it. If you need the position of an item later
+     * on (e.g. in a click listener), use [ViewHolder.getAdapterPosition] which will
+     * have the updated adapter position.
+     *
+     * Override [.onBindViewHolder] instead if Adapter can
+     * handle efficient partial bind.
+     *
+     * @param holder The ViewHolder which should be updated to represent the contents of the
+     * item at the given position in the data set.
+     * @param position The position of the item within the adapter's data set.
+     */
+    override fun onBindViewHolder(holder: RecentTrackViewHolder, position: Int) {
+        if (totalTracks != null) {
+            holder.imgSingleRecentTrack.setImageResource(R.drawable.drawable_cover)
+        }
+    }
+
+    fun setTotalTracks(mTracks: Int) {
+        totalTracks = mTracks
+        notifyDataSetChanged()
+    }
+
+    /**
+     * Returns the total number of items in the data set held by the adapter.
+     *
+     * @return The total number of items in this adapter.
+     */
+    override fun getItemCount(): Int {
+        return if (totalTracks != null)
+            totalTracks!!;
+        else 0;
+    }
+}
