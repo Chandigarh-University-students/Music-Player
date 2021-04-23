f10df1643ab8e2e1027cc2ca9e153d596fd10e1d
Aman Raj <amanr11314@gmail.com>
2021-04-23 22:39:56 +0530

    :heavy_plus_sign: create playlist ui done

--- app/src/main/java/com/projects/musicplayer/fragments/PlaylistsFragment.kt
+++ app/src/main/java/com/projects/musicplayer/fragments/PlaylistsFragment.kt
@@ -1,25 +1,37 @@
 package com.projects.musicplayer.fragments
 
+import android.app.Dialog
 import android.content.Context
+import android.content.DialogInterface
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
+import androidx.core.widget.doOnTextChanged
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.projects.musicplayer.adapters.PlaylistAdapter
 import com.projects.musicplayer.rest.PlaylistModel
 import com.projects.musicplayer.R
+import com.projects.musicplayer.uicomponents.CustomDialog
+import kotlinx.android.synthetic.main.playlist_dialog.*
 
 
 class PlaylistsFragment : Fragment() {
-    
+
     lateinit var recyclerViewPlaylists: RecyclerView
     lateinit var recylcerViewPlaylistadapter: PlaylistAdapter
     lateinit var toolbar: Toolbar
+    lateinit var fabCreatePlaylist: FloatingActionButton
+    lateinit var playlistInputDialog: CustomDialog
 
 
     override fun onCreateView(
@@ -27,14 +39,37 @@ class PlaylistsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-         val view = inflater.inflate(R.layout.fragment_playlists, container, false)
-        recyclerViewPlaylists=view.findViewById(R.id.recyclerViewPlaylists)
+        val view = inflater.inflate(R.layout.fragment_playlists, container, false)
+        recyclerViewPlaylists = view.findViewById(R.id.recyclerViewPlaylists)
         toolbar = view.findViewById(R.id.toolbar)
-        toolbar.title="PlaylistsFragment"
+        fabCreatePlaylist = view.findViewById(R.id.fabCreatePlaylist)
+        playlistInputDialog = CustomDialog(activity as Context)
+        toolbar.title = "PlaylistsFragment"
+
+        fabCreatePlaylist.setOnClickListener {
+            playlistInputDialog.show()
+        }
+
+        playlistInputDialog.positiveButtonCallback = fun(playlistName: String) {
+            if (playlistName.isNotBlank()) {
+                //TODO: CREATE NEW PLAYLIST
+                Toast.makeText(
+                    activity as Context,
+                    "$playlistName playlist created ",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                Toast.makeText(
+                    activity as Context,
+                    "Discarding Empty Playlist Name",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
 
         if (activity != null) {
 
-            val playlists=listOf(
+            val playlists = listOf(
                 PlaylistModel("Playlist 1"),
                 PlaylistModel("Playlist 2"),
                 PlaylistModel("Playlist 3"),
@@ -49,7 +84,17 @@ class PlaylistsFragment : Fragment() {
                 PlaylistModel("Playlist 12"),
                 PlaylistModel("Playlist 13"),
                 PlaylistModel("Playlist 14"),
-                PlaylistModel("Playlist 15")
+                PlaylistModel("Playlist 15"),
+                PlaylistModel("Playlist 16"),
+                PlaylistModel("Playlist 17"),
+                PlaylistModel("Playlist 18"),
+                PlaylistModel("Playlist 19"),
+                PlaylistModel("Playlist 20"),
+                PlaylistModel("Playlist 21"),
+                PlaylistModel("Playlist 22"),
+                PlaylistModel("Playlist 23"),
+                PlaylistModel("Playlist 24")
+
             )
 
             recylcerViewPlaylistadapter =
@@ -71,5 +116,6 @@ class PlaylistsFragment : Fragment() {
         return view
     }
 
-    
+
 }
+
