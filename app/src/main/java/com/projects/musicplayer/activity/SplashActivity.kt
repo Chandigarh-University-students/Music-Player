ac3fe2fddc5652adb1648cb2245054748924618c
Rishabh Kumar Singh <rishabhkumar812@gmail.com>
2021-04-27 12:49:01 +0530

    Improving UI

--- app/src/main/java/com/projects/musicplayer/activity/SplashActivity.kt
+++ app/src/main/java/com/projects/musicplayer/activity/SplashActivity.kt
@@ -1,12 +1,23 @@
 package com.projects.musicplayer.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.projects.musicplayer.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(
+                Intent(this@SplashActivity,
+                    MainActivity::class.java)
+            )
+            finish()
+        }, 2000)
     }
 }
