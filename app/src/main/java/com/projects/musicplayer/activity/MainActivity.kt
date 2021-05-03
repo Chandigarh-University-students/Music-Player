5dc09f69cd6b1db8fd5deb442d6676655f481bcb
Rishabh Kumar Singh <rishabhkumar812@gmail.com>
2021-05-04 03:11:50 +0530

    Exception handle for song deletion from external storage.

--- app/src/main/java/com/projects/musicplayer/activity/MainActivity.kt
+++ app/src/main/java/com/projects/musicplayer/activity/MainActivity.kt
@@ -23,6 +23,7 @@ import com.google.android.material.shape.MaterialShapeDrawable
 import com.projects.musicplayer.R
 import com.projects.musicplayer.database.recentSongs.RecentSongEntity
 import com.projects.musicplayer.database.allSongs.SongEntity
+import com.projects.musicplayer.database.playlists.PlaylistConverter
 import com.projects.musicplayer.fragments.*
 import com.projects.musicplayer.fragments.FavFragment
 import com.projects.musicplayer.fragments.SinglePlaylistFragment
@@ -30,10 +31,13 @@ import com.projects.musicplayer.uicomponents.RepeatTriStateButton
 import com.projects.musicplayer.viewmodel.allSongs.AllSongsViewModel
 import com.projects.musicplayer.viewmodel.allSongs.AllSongsViewModelFactory
 import com.projects.musicplayer.viewmodel.mediaControl.MediaControlViewModel
+import com.projects.musicplayer.viewmodel.playlists.PlaylistViewModel
+import com.projects.musicplayer.viewmodel.playlists.PlaylistViewModelFactory
 import com.projects.musicplayer.viewmodel.recentSongs.RecentSongsViewModel
 import com.projects.musicplayer.viewmodel.recentSongs.RecentSongsViewModelFactory
 import com.squareup.picasso.Picasso
 import kotlinx.coroutines.*
+import java.lang.Exception
 import java.lang.Long.parseLong
 import java.lang.Runnable
 import java.text.DateFormat
@@ -100,13 +104,20 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var mMediaControlViewModel: MediaControlViewModel
 
+    private lateinit var mPlaylistViewModel: PlaylistViewModel
+    private lateinit var mPlaylistViewModelFactory: PlaylistViewModelFactory
+
     lateinit var mediaPlayer: MediaPlayer
     lateinit var runnable: Runnable
 
+
     val MediaPlayer.seconds: Int
-        get() = this.duration / 1000
+        get() = try{Log.e("Refresh","MediaPlayer.seconds is called"); this.duration / 1000}catch (e:Exception){Log.e("Refresh","Media player was null for MediaPlayer.seconds"); 0}
     val MediaPlayer.currentSeconds: Int
-        get() = this.currentPosition / 1000
+        get() = try{Log.e("Refresh","MediaPlayer.currentSeconds is called"); this.currentPosition / 1000}catch (e:Exception){Log.e("Refresh","Media player was null for MediaPlayer.currentSeconds"); 0}
+
+
+    //get() = this.currentPosition / 1000
     var homeFragment: HomeFragment = HomeFragment()
     var handler: Handler = Handler((Looper.getMainLooper()))
 
@@ -160,14 +171,17 @@ class MainActivity : AppCompatActivity() {
             Context.MODE_PRIVATE
         )
 
-        //init View Model obejct
+        //init View Model object
         //use of view model factory to pass parameter to view model
+        /** Viewmodel for AllSongs*/
         mAllSongsViewModelFactory =
             AllSongsViewModelFactory(
                 application
             )
         mAllSongsViewModel =
             ViewModelProvider(this, mAllSongsViewModelFactory).get(AllSongsViewModel::class.java)
+
+        /** Viewmodel for MediaControl*/
         mMediaControlViewModel =
             ViewModelProvider(this).get(MediaControlViewModel::class.java)
 
@@ -178,6 +192,14 @@ class MainActivity : AppCompatActivity() {
             )
         mRecentSongsViewModel =
             ViewModelProvider(this, mRecentSongsViewModelFactory).get(RecentSongsViewModel::class.java)
+
+        /** Viewmodel for playlists*/
+        mPlaylistViewModelFactory =
+            PlaylistViewModelFactory(
+                application
+            )
+        mPlaylistViewModel =
+            ViewModelProvider(this, mPlaylistViewModelFactory).get(PlaylistViewModel::class.java)
 
 
         mAllSongsViewModel.allSongs.observe(this, Observer {
@@ -260,21 +282,35 @@ class MainActivity : AppCompatActivity() {
         mMediaControlViewModel.nowPlayingSong.observe(this, Observer {
             Log.i("PLAYLISTSONG", "New Song Clicked ${it.songName}")
             Log.i("NEXTPREV", mMediaControlViewModel.isFirstInit.value!!.toString())
-            setUpMediaPlayer(it, !mMediaControlViewModel.isFirstInit.value!!)
-            initializeSeekbar()
-            mRecentSongsViewModel.updateRecentSong(
-                RecentSongEntity(
-                    it.songId,
-                    it.albumId,
-                    getLocalTime()
+            if(setUpMediaPlayer(it, !mMediaControlViewModel.isFirstInit.value!!)){
+                Log.e("Refresh","setupMediaPlayer returned with true value")
+                initializeSeekbar()
+                mRecentSongsViewModel.updateRecentSong(
+                    RecentSongEntity(
+                        it.songId,
+                        it.albumId,
+                        getLocalTime()
+                    )
                 )
-            )
-            uiscope.launch {
-                setUpCollapsedBottomSheetUI(it)
+                uiscope.launch {
+                    setUpCollapsedBottomSheetUI(it)
+                }
+                uiscope.launch {
+                    setUpExpandedBottomSheetUI(it)
+                }
+            }else{
+                Log.e("Refresh","setupMediaPlayer returned with false value")
+                //TODO Now refresh the whole database
+                Toast.makeText(this,"Song has been deleted from storage!!",Toast.LENGTH_SHORT).show()
+                runBlocking {
+                    progressLayout.visibility=View.VISIBLE
+                    Log.i("Refresh","...............................Start method............................")
+                    refreshDatabases()
+                    progressLayout.visibility=View.GONE
+                    Log.i("Refresh","...............................Stop method............................")
+                }
             }
-            uiscope.launch {
-                setUpExpandedBottomSheetUI(it)
-            }
+
         })
 
         mMediaControlViewModel.nowPlayingSongs.observe(this, Observer {
@@ -373,12 +409,22 @@ class MainActivity : AppCompatActivity() {
         return localTime
     }
 
-    fun setUpMediaPlayer(songEntity: SongEntity, toPlay: Boolean = true) {
+    fun setUpMediaPlayer(songEntity: SongEntity, toPlay: Boolean = true):Boolean {
         clearMediaPlayer()
         val songUri = ContentUris.withAppendedId(
             MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, songEntity.songId.toLong()
         )
-        mediaPlayer = MediaPlayer.create(applicationContext, songUri)
+        Log.e("Refresh","Song uri now is $songUri")
+        Log.e("Refresh","mediaPlayer intialized = ${this::mediaPlayer.isInitialized}")
+        try{
+            mediaPlayer = MediaPlayer.create(applicationContext, songUri)
+        }catch (e:Exception){
+            //TODO delete this song from everywhere,print a toast message
+            Log.e("Refresh","Media player was null inside setUpMediaPlayer")
+            Log.e("Refresh","mediaPlayer intialized = ${this::mediaPlayer.isInitialized}")
+            return false
+        }
+
         if (toPlay)
             uiscope.launch {
                 mMediaControlViewModel.isPlaying.value = true
@@ -434,18 +480,23 @@ class MainActivity : AppCompatActivity() {
             }
 
         }
+        return true
 }
 
     fun initializeSeekbar() {
 
-        controlSeekBar.max = mediaPlayer.seconds
-        txtTotalDuration.text = getDuration(mediaPlayer.seconds.toLong())
-        runnable = Runnable {
-            controlSeekBar.progress = mediaPlayer.currentSeconds
-            txtCurrentDuration.text = getDuration(mediaPlayer.currentSeconds.toLong())
+        try{
+            controlSeekBar.max = mediaPlayer.seconds
+            txtTotalDuration.text = getDuration(mediaPlayer.seconds.toLong())
+            runnable = Runnable {
+                controlSeekBar.progress = mediaPlayer.currentSeconds
+                txtCurrentDuration.text = getDuration(mediaPlayer.currentSeconds.toLong())
+                handler.postDelayed(runnable, 1000)
+            }
             handler.postDelayed(runnable, 1000)
+        }catch (e:Exception){
+            Log.e("Refresh","Media player was null inside seekbar")
         }
-        handler.postDelayed(runnable, 1000)
     }
 
     fun getDuration(seconds: Long): String {
@@ -723,10 +774,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun clearMediaPlayer() {
-        if (this::mediaPlayer.isInitialized) {
-            mediaPlayer.stop()
-            mediaPlayer.reset()
-            mediaPlayer.release()
+        try{
+            if (this::mediaPlayer.isInitialized) {
+                mediaPlayer.stop()
+                mediaPlayer.reset()
+                mediaPlayer.release()
+            }
+        }catch (e:Exception){
+            Log.e("Refresh","Media player was null inside clearMediaPlayer")
         }
     }
 
@@ -919,6 +974,158 @@ class MainActivity : AppCompatActivity() {
 
         } else {
             return
+        }
+    }
+
+    private fun refreshDatabases()
+    {
+        //TODO fetch again and set all databases
+        var oldSongsList:MutableList<SongEntity>
+        runBlocking{
+            oldSongsList = mAllSongsViewModel.getAllSongs() as MutableList<SongEntity>
+        }
+        var newSongsList = prepareRefresh(ContextWrapper(applicationContext).contentResolver)
+        var sameSongs = mutableListOf<SongEntity>()
+        var newAddedSongs = mutableListOf<SongEntity>()
+        var deletedSongs = mutableListOf<SongEntity>()
+
+        Log.i("Refresh","Old song list size - ${oldSongsList.size}")
+        Log.i("Refresh","New song list size - ${newSongsList.size}")
+
+        for(song in newSongsList){
+            val songComplement =
+                SongEntity(
+                    song.songId,
+                    song.songName,
+                    song.artistName,
+                    song.duration,
+                    song.albumId,
+                    song.isFav * (-1)
+                )
+            if(oldSongsList.remove(song) || oldSongsList.remove(songComplement)){
+                sameSongs.add(song)
+            }else{
+                newAddedSongs.add(song)
+            }
+        }
+        deletedSongs = oldSongsList
+        Log.i("Refresh","Common song list size - ${sameSongs.size}")
+        Log.i("Refresh","New added song list  - $newAddedSongs")
+        Log.i("Refresh","Deleted song list  - $deletedSongs")
+
+        //TODO Now reset databases according to added and deleted songs
+        /**Setting database for newly added songs**/
+        mAllSongsViewModel.insertSongs(newAddedSongs)
+        /**Both play queue and home adapter will be reloaded*/
+
+        /**Setting database for deleted songs**/
+
+        for(song in deletedSongs){
+            Log.i("Refresh","Deleting song  - $song")
+
+            /** Removing form AllSongs*/
+            mAllSongsViewModel.removeSong(song)
+
+            /** Removing form RecentSongs*/
+            mRecentSongsViewModel.deleteRecentSong(RecentSongEntity(song.songId,song.albumId,getLocalTime()))
+
+            /** Removing form Playlists*/
+            var playlistIdList : List<Int>
+            runBlocking {
+                playlistIdList = mPlaylistViewModel.getAllPlaylists()
+            }
+            for(playlistId in playlistIdList) {
+                Log.i("Refresh","Deleting song $song from playlist $playlistId")
+                var songs: String? = "Sample"
+                runBlocking {
+                    songs = mPlaylistViewModel.getPlaylistSongsById(playlistId)
+                }
+                uiscope.launch {
+                    val listOfSongs: List<Int>? = PlaylistConverter.toList(songs)
+                    if (listOfSongs == null)
+                        Log.e("Refresh", "Empty Playlist")
+                    else {
+                        val mutableSongs = (listOfSongs as MutableList<Int>)
+                        if(mutableSongs.remove(song.songId)){
+                            Log.i("Refresh", "${song.songName} found in $playlistId")
+                            mPlaylistViewModel.updatePlaylist(playlistId, mutableSongs)
+                        }else{
+                            Log.i("Refresh", "${song.songName} not found in $playlistId")
+                        }
+                    }
+                }
+            }
+
+            /** Removing form NowPlaying*/
+            if (song.songId == mMediaControlViewModel.nowPlayingSong.value?.songId ) {
+                Log.i("Refresh", "Deleted song also in Now Playing")
+                var allSongs : List<SongEntity>
+                runBlocking {
+                    allSongs = mAllSongsViewModel.getAllSongs()
+                }
+                if(!allSongs.isNullOrEmpty()) {
+                    Log.i("Refresh", "Setting now playing song as random song from all songs")
+                    runBlocking {
+                        if(mMediaControlViewModel.isPlaying.value==false){
+                            Log.i("Refresh", "song was paused already")
+                            mMediaControlViewModel.isFirstInit.value = true
+                        }else{
+                            Log.i("Refresh", "song was played already")
+                        }
+                    }
+                    runBlocking {
+                        mMediaControlViewModel.nowPlaylist.value = "All Songs"
+                        mMediaControlViewModel.nowPlayingSongs.value = allSongs
+                        mMediaControlViewModel.nowPlayingSong.value = allSongs.random()
+                    }
+                }
+                else{
+                    Log.i("Refresh", "All songs empty message should be on screen automatically")
+                }
+            }
+        }
+    }
+
+    private fun prepareRefresh(mContentResolver: ContentResolver) : MutableList<SongEntity>{
+
+        val mSongs = mutableListOf<SongEntity>()
+        val uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+        val cur: Cursor? = mContentResolver.query(
+            uri,
+            null,
+            MediaStore.Audio.Media.IS_MUSIC + "!= 0",
+            null,
+            MediaStore.Audio.Media.TITLE + " ASC"
+        )
+
+        if (cur != null && cur.moveToFirst()) {
+            val albumArtColumn = cur.getColumnIndex(MediaStore.Audio.Media.ALBUM_ID)
+            val durationColumn = cur.getColumnIndex(MediaStore.Audio.Media.DURATION)
+            val artistColumn = cur.getColumnIndex(MediaStore.Audio.Media.ARTIST)
+            val titleColumn = cur.getColumnIndex(MediaStore.Audio.Media.TITLE)
+            val idColumn = cur.getColumnIndex(MediaStore.Audio.Media._ID)
+            do {
+                mSongs.add(
+                    SongEntity(
+                        cur.getInt(idColumn),
+                        cur.getString(titleColumn),
+                        cur.getString(artistColumn),
+                        cur.getLong(durationColumn),
+                        cur.getLong(albumArtColumn).toString(),
+                        -1
+                    )
+                )
+            } while (cur.moveToNext())
+            cur.close()
+
+            //TODO mSongs has all updated songs
+            return mSongs
+            //mAllSongsViewModel.insertSongs(mSongs)
+            //sharedPreferences.edit().putBoolean("songLoaded", true).apply()
+
+
+        } else {
+            return mSongs
         }
     }
 
